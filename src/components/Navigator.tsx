e9f93fc7f0478594e18f8574fe676ce0bd9e1b4b
ammar <ammar.ali.haidar.1990@gmail.com>
2023-06-13 12:14:19 +0300

    done

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -6,7 +6,7 @@ import { CodeBracketIcon } from "@heroicons/react/24/outline";
 import { TrophyIcon } from "@heroicons/react/24/outline";
 import { CommandLineIcon } from "@heroicons/react/24/outline";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import {Bars3Icon} from '@heroicons/react/24/outline'
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -19,17 +19,15 @@ const Navigator = ({ theHash }: Props) => {
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if (!(e.target as HTMLElement).classList.contains('navigator')) {
+      if (!(e.target as HTMLElement).classList.contains("navigator")) {
         setOpen(false);
       }
-
-
     };
 
-    window.addEventListener('click', handleClickOutside);
+    window.addEventListener("click", handleClickOutside);
 
     return () => {
-      window.removeEventListener('click', handleClickOutside);
+      window.removeEventListener("click", handleClickOutside);
     };
   }, [setOpen]);
 
@@ -59,7 +57,7 @@ const Navigator = ({ theHash }: Props) => {
       to: "#certifications",
       Icon: TrophyIcon,
     },
-    
+
     {
       name: "projects",
       to: "#projects",
@@ -74,24 +72,35 @@ const Navigator = ({ theHash }: Props) => {
 
   return (
     <div
-      className={` ${
+      className={`navigator cursor-pointer ${
         theHash !== "" && "opacity-100 pointer-events-auto"
-      } delay-300 opacity-0 pointer-events-none duration-500 fixed top-5 w-fit flex-col rounded-md p-3 ${open && 'gap-4'}  bg-main/40 text-white mx-auto flex justify-center items-center  left-0 right-0 z-50`}
+      } delay-300 opacity-0 pointer-events-none duration-500 fixed top-5 w-fit flex-col rounded-sm p-1  ${
+        open && "gap-4"
+      }  bg-main/40 text-white mx-auto flex justify-center items-center  left-0 right-0 z-50`}
+      onClick={() => setOpen((prev) => !prev)}
     >
-      <Bars3Icon className="navigator h-4 text-white cursor-pointer selfe " onClick={()=>setOpen(prev=>!prev)}/>
-      <div className={`flex  gap-1 items-center ${!open ? 'w-0 h-0 ' :" w-[195px] h-10"} overflow-hidden duration-300`}>
-      {tabs.map(({ Icon, to, name }, i) => (
-        <Link
-        onClick={()=>setOpen(false)}
-          key={i}
-          className={`${theHash === name && "text-red-600 bg-white rounded-full "}block ${open && "p-1"}`}
-          href={to}
-        >
-          {<Icon className=" h-4" />}
-        </Link>
-      ))}
+      <Bars3Icon
+        className="navigator h-4 text-white   "
+        
+      />
+      <div
+        className={`flex  gap-1 items-center ${
+          !open ? "w-0 h-0 " : " w-[195px] h-10"
+        } overflow-hidden duration-300`}
+      >
+        {tabs.map(({ Icon, to, name }, i) => (
+          <Link
+            onClick={() => setOpen(false)}
+            key={i}
+            className={`${
+              theHash === name && "text-red-600 bg-white rounded-full "
+            }block ${open && "p-1"}`}
+            href={to}
+          >
+            {<Icon className=" h-4" />}
+          </Link>
+        ))}
       </div>
-    
     </div>
   );
 };
