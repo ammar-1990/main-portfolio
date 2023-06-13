3d7ceedc7bb34c524f2d7a653b12787c287d5ff6
ammar <ammar.ali.haidar.1990@gmail.com>
2023-06-13 14:37:12 +0300

    done

--- src/data.js
+++ src/data.js
@@ -222,22 +222,22 @@ export const certificates = [
     {
       id: "c1",
       name: "google project management",
-      icon: "/Images/google.png",
-      img: "/Images/min.JPG",
+      icon: "/images/google.png",
+      img: "/images/min.JPG",
       link: "https://www.coursera.org/account/accomplishments/professional-cert/AY8F2BJLHAB5",
     },
     {
       id: "c2",
       name: "meta frontend developer",
-      icon: "/Images/Meta.png",
-      img: "/Images/min2.JPG",
+      icon: "/images/Meta.png",
+      img: "/images/min2.JPG",
       link: "https://www.coursera.org/account/accomplishments/professional-cert/A3MKYTPLGJ28",
     },
     {
       id: "c3",
       name: "meta react native",
-      icon: "/Images/Meta.png",
-      img: "/Images/min3.JPG",
+      icon: "/images/Meta.png",
+      img: "/images/min3.JPG",
       link: "https://www.coursera.org/account/accomplishments/specialization/G8XCADNXZDGR",
     },
   ];
@@ -247,7 +247,7 @@ export const certificates = [
     {
       id: "1",
       name: "Fitness compass",
-      img: "/Images/compass-fitness-min.png",
+      img: "/images/compass-fitness-min.png",
       pull: true,
       overView:
         "This is my first project,here we can search for different meals according to our caloric needs,we can also filtiring the results using an advanced search option.so if want a certain cusine we can explicitly choose that,or if have some kind of intolerance to food ,so we can specify that also.There is a right side bar for calculating our caloric needs according to our informations.The results are shown in beautiful cards whith an image and more info to see in a pop up modal that holds all the information about the meal we choose",
@@ -258,7 +258,7 @@ export const certificates = [
     {
       id: "2",
       name: "E-commerce",
-      img: "/Images/js-e-commerce-min.png",
+      img: "/images/js-e-commerce-min.png",
       pull: false,
       overView:
         "This is my second project,here we can search for different products and items ,and adding them to the cart with a full control of increasing decreasing the amount of choosen items and deleting them partialy or totally .The data is fully dynamic through a third party API.The items are shown in nice cards with images ,info,and see more button to show additional informations in a pop up modal",
@@ -269,7 +269,7 @@ export const certificates = [
     {
       id: "3",
       name: "Lost pets",
-      img: "/Images/lost-pets-min.png",
+      img: "/images/lost-pets-min.png",
       pull: false,
       overView:
         "In this site we can register new users ,sign in and out.The whole idea of this site is to register and share lost or found animals which we want to find thier owners .We can do that by posting new animal info on a shared wall between the users specifying an image and additional details of the animal .The users then can interact with each other through different types of messages that track the state of the animal.Either the animal is found or not yet ,the result would be tracked and saved in the site",
@@ -280,7 +280,7 @@ export const certificates = [
     {
       id: "4",
       name: "React e-commerce",
-      img: "/Images/yellow-e-commerce-min.png",
+      img: "/images/yellow-e-commerce-min.png",
       pull: true,
       overView:
         "A fully responsive E-commerce web site with nice and beautiful colors , layout,and a lot of products illustrations having the ability to show ,swipe and filter them properly .This project was built using React js ,Swiper js , Framer motion , and Sass",
@@ -291,7 +291,7 @@ export const certificates = [
     {
       id: "5",
       name: "Fitness club",
-      img: "/Images/fitness-club-min.png",
+      img: "/images/fitness-club-min.png",
       pull: true,
       overView:
         "A fully responsive Fitness-club web site with nice and beautiful colors , layout,and many images and illustrations that describe the environment and the sevices of the club.This project was built using React js ,Framer motion and Sass",
@@ -302,7 +302,7 @@ export const certificates = [
     {
       id: "6",
       name: "Admin dashboard",
-      img: "/Images/dashboard-min.png",
+      img: "/images/dashboard-min.png",
       pull: false,
       overView:
         "Fully responsive dashboard web site with beautiful colors and layout,having the ability to switch between light and dark mode.This project was built using React js ",
@@ -313,7 +313,7 @@ export const certificates = [
     {
       id: "7",
       name: "Todo list",
-      img: "/Images/todo-list-min.png",
+      img: "/images/todo-list-min.png",
       pull: false,
       overView:
         "Basic Todo project with the ability to add ,edit , remove and filter the  data with nice animations and a good layout .The data is stored in the local storage of the browser and managed with Redux toolkit",
@@ -324,7 +324,7 @@ export const certificates = [
     {
       id: "8",
       name: "Redux e-commerce",
-      img: "/Images/redux-e-commerce-min.png",
+      img: "/images/redux-e-commerce-min.png",
       pull: true,
       overView:
         "Fully responsive E-commerce project with a fully functional cart to add ,remove and edit the state of our choosen products .We can easily navigate between differnt pages and hit to the details page of the product with nice animated transitions.This project was built using React js ,Redux toolkit,Framer motion and Reacr Router Dom",
@@ -340,7 +340,7 @@ export const certificates = [
     {
       id: "9",
       name: "Netflix clone",
-      img: "/Images/netflix-clone-min.png",
+      img: "/images/netflix-clone-min.png",
       pull: true,
       overView:
         "A NETFLIX clone with the ability to sign in and out using  firebase database ,the site is fully responsive with a lot of dynamic movies informations using third party APIs,having the ability to access detailed info about each movie.the project was built using React js ,React Router Dom ,Firebase ",
@@ -351,7 +351,7 @@ export const certificates = [
     {
       id: "10",
       name: "Facebook clone",
-      img: "/Images/facebook-clone-min.png",
+      img: "/images/facebook-clone-min.png",
       pull: false,
       overView:
         "In this Facebook clone we can create new users ,sign in and out ,adding new posts ,comments and likes,we can also receive notifications from other users,adding images and modify personal details.So it is a fully active site which gives the experience of the real Fasebook site.this project was built using Next Js ,firebase with a fully controlled database",
@@ -362,7 +362,7 @@ export const certificates = [
     {
       id: "11",
       name: "Real estate",
-      img: "/Images/real-estate-min.png",
+      img: "/images/real-estate-min.png",
       pull: true,
       overView:
         "Fully responsive MPA  real estate site which illustrates a lot of dynamic options using third party APIs.The data is shown as a modern beautiful cards ,using swiper to give the ability to choose and navigate freely,each card caontains the details and specifications of the real estate with the ability to navigate instantly to the details page.This project was built using Next js ,Swiper and form validation library",
@@ -379,7 +379,7 @@ export const certificates = [
     {
       id: "12",
       name: "Dashboard - auth",
-      img: "/Images/dashboard-auth.png",
+      img: "/images/dashboard-auth.png",
       pull: false,
       overView:
         "This admin dashboard was designed using react js and firebase ,so we have the ability to authenticate new created users and  to add ,edit and remove products or other normal users through a fully functional CRUD operations.This site illustrates many tabels and charts using material ui components ,and gives the the ability to switch between light and dark mode",
@@ -390,7 +390,7 @@ export const certificates = [
     {
       id: "13",
       name: "Next-Dashboard",
-      img: "/Images/next-dash-board.png",
+      img: "/images/next-dash-board.png",
       pull: false,
       overView:
         "This dashboard is only larg screens friendly ,designed in a beautiful dark mode using Nextjs and many other libraries ,like echarts,material tables,and react full callendar,consists of 3 main pages with a lot of functionality",
@@ -401,7 +401,7 @@ export const certificates = [
     {
       id: "14",
       name: "Amazon-clone",
-      img: "/Images/amazon.png",
+      img: "/images/amazon.png",
       pull: true,
       overView:
         "A complete amazon clone with the ability to add products to cart and remove them, excute payments and observe our orders ,the site is fully responsive and was built with next js ,tailwind ,and redux toolkit",
@@ -412,7 +412,7 @@ export const certificates = [
     {
       id: "15",
       name: "Airbnb Clone",
-      img: "/Images/airbnb.png",
+      img: "/images/airbnb.png",
       pull: true,
       overView:
         "Fully responsive airbnb clone built with next js using get server side and get static props with the ability to use date range in search input to specify our information ,with a progress bar when navigating between pages",
@@ -423,7 +423,7 @@ export const certificates = [
     {
       id: "16",
       name: "Instagram clone",
-      img: "/Images/insta.png",
+      img: "/images/insta.png",
       pull: true,
       overView:
         "Fully responsive Instagram clone built with Next js Firebase and tailwind with the ability to create new users, authentication, add posts , add comments and likes in addition to profile and post images with a fully checked signin and signout operation",
@@ -434,7 +434,7 @@ export const certificates = [
     {
       id: "17",
       name: "NextJS-13 News App",
-      img: "/Images/next13-app.png",
+      img: "/images/next13-app.png",
       pull: true,
       overView:
         "Fully responsive News App whith Nextjs 13 ,using 3rd party API to fetch real data ,By implementing ServerSide rendering , caching data ,and getStaticParams,the data is categoriezed and could be fetched dynamically using search input,we can switch between dark and ligh mode using next-theme.",
@@ -445,7 +445,7 @@ export const certificates = [
     {
       id: "18",
       name: "NextJS Reddit Clone",
-      img: "/Images/reddit.png",
+      img: "/images/reddit.png",
       pull: false,
       overView:
         "In this project, I have built a fully functional subreddit clone using Next.js, Supabase, and Tailwind. The clone allows users to create new posts, add comments to existing posts, and vote on posts. Additionally, it features a subreddit page where users can browse all posts related to a particular subreddit. I used Next.js to build a fast and efficient web application that delivers a seamless user experience. Leveraging the power of Supabase, I was able to store and manage data in a reliable and scalable way. And with the help of Tailwind, I created a visually appealing and responsive user interface that looks great on any device.",
@@ -456,7 +456,7 @@ export const certificates = [
     {
       id: "19",
       name: "NextJS Whats App  Clone",
-      img: "/Images/whatsapp.png",
+      img: "/images/whatsapp.png",
       pull: false,
       overView:
         "This is a real-time messaging app built with Next-js and Firebase 9. Users can register new accounts, log in, and log out. Once logged in, they can create new chats by entering the email address of the user they want to chat with. The app uses Firebase Realtime Database to store chat messages and sync them in real time between users",
@@ -467,7 +467,7 @@ export const certificates = [
     {
       id: "20",
       name: "MERN Fiverr Clone",
-      img: "/Images/fiverr.png",
+      img: "/images/fiverr.png",
       pull: true,
       overView:
         "Full-stack Fiverr clone using the MERN stack, featuring user registration and authentication with JWT and cookies, gig creation and order placement, and messaging between buyers and sellers. The application included gig filtering and search capabilities to help users find the services they need.The front-end of the application was built using React, and the back-end was implemented using Node.js and Express. MongoDB was used for data storage and retrieval. JSON Web Tokens (JWT) and cookies were used for user authentication and security. Throughout the project, I gained valuable experience in developing a full-stack web application and working with technologies such as React, Node.js, Express, MongoDB, and JWT. I also learned about the importance of user authentication and security in web applications, and how to implement these features using JWT and cookies.",
@@ -486,7 +486,7 @@ export const certificates = [
     {
       id: "21",
       name: "MERN Chat-App",
-      img: "/Images/alpha.png",
+      img: "/images/alpha.png",
       pull: false,
       overView:
         "Real-time chat application with WebSocket. The application allows users to register, log in, and send instant messages to other users in real-time. Using React for the frontend, Node.js and Express for the backend, and MongoDB for the database, I developed a responsive and user-friendly chat interface that enables easy communication between users. The WebSocket technology ensures that messages are delivered instantly and without delay. To ensure secure user authentication, I implemented a robust login and registration system using JSON Web Tokens (JWT) for user authorization. This feature ensures that only authenticated users can access the chat system.",
@@ -505,7 +505,7 @@ export const certificates = [
     {
       id: "22",
       name: "MERN Blog-App",
-      img: "/Images/mern-blog.png",
+      img: "/images/mern-blog.png",
       pull: true,
       overView:
         " I have created a fully functional blog website that allows users to create, edit, and delete posts, filter them by categories, and perform login and logout operations. The website is built using the latest technologies, including React, Node.js, Express, and MongoDB. Here are some of the key features of the website. User authentication: Users can create accounts, log in, and log out of the website. Authentication is handled securely using JWT tokens.CRUD operations: Users can create, read, update, and delete their own posts. The website also has an admin panel that allows the administrator to manage all the posts on the website. Filtering by categories: Users can filter posts by categories, making it easy to find the posts they are interested in.Responsive design: The website is designed to look great on all devices, including desktops, tablets, and smartphones. Overall, this project showcases my skills as a MERN full-stack developer and demonstrates my ability to build complex web applications from the ground up.",
@@ -524,7 +524,7 @@ export const certificates = [
     {
       id: "23",
       name: "NEXT js E_COMMERCE DASHBOARD",
-      img: "/Images/next-dashboard.png",
+      img: "/images/next-dashboard.png",
       pull: false,
       overView:
         "I developed a full-stack dashboard web application using Next.js, MongoDB, and Tailwind. The dashboard provides administrators with full control over the application's products, categories, and orders. Administrators can register, login, and manage the application's products by adding, editing, and deleting them. They can also manage the application's categories and view all orders placed by users.This project demonstrates my ability to develop complex back-end applications using modern tools and technologies. I utilized Next.js for server-side rendering, MongoDB for data storage, and Tailwind for styling. The dashboard's intuitive and user-friendly interface showcases my proficiency in creating applications that meet the unique needs of administrators.",
@@ -541,7 +541,7 @@ export const certificates = [
     {
       id: "24",
       name: "NEXT js E_COMMERCE WEBSITE",
-      img: "/Images/next-ecommerce-front.png",
+      img: "/images/next-ecommerce-front.png",
       pull: true,
       overView:
         "I developed a full-stack web application using Next.js, MongoDB, Redux, and Tailwind. The application allows users to browse the latest products, all products, and categories, as well as filter products by category, add and remove products from their cart, and place orders. The application's state is managed by Redux, providing a seamless and responsive user experience. This project demonstrates my ability to develop complex full-stack applications using modern tools and technologies. I utilized Next.js for server-side rendering, MongoDB for data storage, Redux for state management, and Tailwind for styling. The front-end's intuitive and user-friendly interface showcases my proficiency in creating applications that meet the unique needs of end-users.",
